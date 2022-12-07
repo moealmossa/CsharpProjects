0fb1615df77c2ed7e90cb721ed61aeec25e0623f
moealmossa <moalmossa20@gmail.com>
2022-12-07 19:29:58 +0300

    done

---
+++ TypeConverstionHomeWork/TypeConverstionHomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TypeCoverstionHomeWork 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you");
+            string ageText = Console.ReadLine();
+
+            int age = Convert.ToInt32(ageText);
+            
+
+            Console.WriteLine($"you will be {age+25} in 25 years,\n but you were {age-25} 25 years ago");
+
+        }
+    }
+}
