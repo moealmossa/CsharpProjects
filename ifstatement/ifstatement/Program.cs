81c18fb69364b7824937db88529f24f704bc4c0a
moealmossa <moalmossa20@gmail.com>
2022-12-10 05:29:21 +0300

    done

---
+++ ifstatement/ifstatement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello what is your name?");
+            string name = Console.ReadLine();
+            if(name == "mansor")
+            {
+                Console.WriteLine($"hello {name} you are a professor");
+            } else
+            {
+                Console.WriteLine($"hello {name} you are a student");
+            }
+        }
+    }
+}
